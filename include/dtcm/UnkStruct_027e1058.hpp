2fa3b4870fe05c3237d1a089f30650e104276efc
Aetias <aetias@outlook.com>
2024-02-22 18:18:48 +0100

    Add DTCM struct definitions

---
+++ include/dtcm/UnkStruct_027e1058.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "types.h"
+
+struct UnkStruct_027e1058 {
+    /* 0 */ const char *unk_0;
+    /* 4 */ s32 unk_4;
+    /* 8 */ s32 unk_8;
+    /* c */
+};
+
+extern UnkStruct_027e1058 data_027e1058;
---
+++ include/dtcm/UnkStruct_027e105c.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "types.h"
+
+struct UnkStruct_027e105c {
+    /* 0 */ const char **unk_0;
+    /* 4 */
+    
+    unk32 LoadTreasureItemFanfare(Treasure);
+};
+
+extern UnkStruct_027e105c data_027e105c;
