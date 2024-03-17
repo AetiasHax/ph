7490c0fc8c2916984a0ec8d6f8cb11e87861a9f8
Aetias <aetias@outlook.com>
2024-03-17 09:27:59 +0100

    Organize source files in overlay directories

--- tools/lcf.py
+++ tools/lcf.py
@@ -24,7 +24,7 @@ DTCM_OBJECTS = [
 
 ov00 = Overlay(name='ov00', after='ARM9', objects=[
     'asm/ov00/ov00_020773c0.s',
-    'src/Item/ItemManager.cpp',
+    'src/00_Core/ItemManager.cpp',
     'asm/ov00/ItemManager/ItemManager.s',
     'asm/ov00/ov00_020ae7a4.s',
     'asm/ov00/ov00_init.s'
