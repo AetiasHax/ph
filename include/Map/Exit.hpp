a2bd2faaf0bb4aa6c652b460d0bdb1f528c7605d
Aetias <aetias@outlook.com>
2024-04-30 15:56:52 +0200

    Update `Exit` fields

--- include/Map/Exit.hpp
+++ include/Map/Exit.hpp
@@ -5,19 +5,14 @@
 
 #include "System/SysNew.hpp"
 
-struct ExitDest {
-    /* 0 */ unk8 mEntrance;
-    /* 1 */ unk8 mMap;
-};
-
 class Exit : public SysObject {
     /* 00 */ unk32 mDestCourse;
     /* 04 */ unk32 mUnk_04;
     /* 08 */ unk32 mUnk_08;
     /* 0c */ unk32 mUnk_0c;
-    /* 10 */ ExitDest mDest;
-    /* 12 */ unk8 mUnk_12;
-    /* 13 */ unk8 mUnk_13;
+    /* 10 */ unk16 mUnk_10;
+    /* 12 */ unk8 mDestMap;
+    /* 13 */ unk8 mDestEntrance;
     /* 14 */ unk8 mUnk_14;
     /* 15 */ unk8 mUnk_15;
     /* 16 */ unk8 mUnk_16;
