33a875477dc9ec2d352b066002ef1c1f38467174
Mike <mikebuntu68@gmail.com>
2024-05-05 16:47:04 -0400

    Add fields to `FilterActorBase`

--- include/Actor/FilterActorBase.hpp
+++ include/Actor/FilterActorBase.hpp
@@ -4,13 +4,28 @@
 #include "types.h"
 
 #include "Actor/ActorRef.hpp"
+#include "Physics/AABB.hpp"
+#include "Physics/Sphere.hpp"
 
 class Actor;
 
 class FilterActorBase {
 public:
-    /* 0 (vtable) */
-    /* 4 */
+    /* 00 (vtable) */
+    /* 04 */ unk32 mType;
+    /* 08 */ unk32 mUnk_08;
+    /* 0c */ unk32 mUnk_0c;
+    /* 10 */ s32 *mExcludeIds;
+    /* 14 */ bool mExcludeNotInAABB;
+    /* 15 */ unk8 mUnk_15;
+    /* 16 */ unk8 mUnk_16;
+    /* 17 */ unk8 mUnk_17;
+    /* 18 */ AABB mAABB;
+    /* 30 */ bool mExcludeNotInSphere;
+    /* 31 */ unk8 mUnk_31;
+    /* 32 */ unk8 mUnk_32;
+    /* 33 */ unk8 mUnk_33;
+    /* 34 */ Sphere mSphere;
 
     /* 0 */ virtual bool Filter(Actor *actor) = 0;
     /* 4 */
