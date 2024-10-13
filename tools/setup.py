cc82956a0dbf7620a8b6bd15d69b92b5584adc1a
Aetias <aetias@outlook.com>
2024-10-13 11:17:41 +0200

    Delete old unused files

--- tools/setup.py
+++ tools/setup.py
@@ -39,13 +39,3 @@ response = requests.get('http://decomp.aetias.com/mwccarm.zip')
 zip_file = zipfile.ZipFile(io.BytesIO(response.content))
 zip_file.extractall(tools_path)
 
-
-print('\nInstalling ELFIO...')
-response = requests.get('https://github.com/serge1/ELFIO/releases/download/Release_3.12/elfio-3.12.zip')
-zip_file = zipfile.ZipFile(io.BytesIO(response.content))
-zip_file.extractall(deps_path)
-elfio_path = deps_path / 'elfio-3.12'
-elfio_new_path = deps_path / 'elfio'
-if elfio_new_path.exists(): shutil.rmtree(elfio_new_path)
-elfio_path.rename(elfio_new_path)
-
