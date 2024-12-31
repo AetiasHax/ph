82ba3ff31176383a6cd7902aaf6e2941d039e02e
mike8699 <mikebuntu68@gmail.com>
2024-12-31 01:21:30 -0500

    Start decompiling `LinkStateItem`

--- src/00_Core/Player/LinkStateItem.cpp
+++ src/00_Core/Player/LinkStateItem.cpp
@@ -1,10 +1,34 @@
+#include "Item/ItemManager.hpp"
 #include "Player/LinkStateItem.hpp"
+#include "Save/AdventureFlags.hpp"
 
 void LinkStateItem::vfunc_00() {}
-LinkStateId LinkStateItem::GetId() {}
-s32 LinkStateItem::IsHammerEquipped() {}
+
+LinkStateId LinkStateItem::GetId() {
+    return 1;
+}
+
+s32 LinkStateItem::IsHammerEquipped() {
+    switch (this->mEquipId) {
+        case 8:
+            return 0;
+        default:
+            return -1;
+    }
+}
+
 void LinkStateItem::OnStateLeave(s32 param1) {}
-EquipBombchu *LinkStateItem::GetEquipBombchu() {}
+
+EquipBombchu *LinkStateItem::GetEquipBombchu() {
+    return (EquipBombchu *)ItemManager::GetEquipItemUnchecked(7);
+}
+
 LinkStateMove *LinkStateItem::GetLinkStateMove() {}
-bool LinkStateItem::func_ov00_020abf70() {}
-bool LinkStateItem::vfunc_28() {}
+
+bool LinkStateItem::func_ov00_020abf70() {
+    return gAdventureFlags->func_ov00_02097b9c(this->mUnk_5c);
+}
+
+bool LinkStateItem::vfunc_28() {
+    return this->mUnk_5c < 0;
+}
