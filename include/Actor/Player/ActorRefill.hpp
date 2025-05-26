d1acad0dd8c597b567376d253284a335a5554bee
Aetias <aetias@outlook.com>
2025-05-26 23:30:28 +0200

    Delink `ActorRefill`

---
+++ include/Actor/Player/ActorRefill.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "global.h"
+#include "types.h"
+
+#include "Actor/Actor.hpp"
+
+class ActorRefill : public Actor {
+public:
+    /* 000 (base) */
+    /* 158 */ unk32 mUnk_158;
+    /* 15c */ unk32 mUnk_15c;
+    /* 160 */ unk32 mUnk_160;
+    /* 164 */ unk8 mUnk_164;
+    /* 165 */ unk8 mUnk_165[0x3];
+    /* 168 */
+
+public:
+    ActorRefill();
+
+    /* 00 */ virtual ~ActorRefill() override;
+    /* 08 */ virtual bool vfunc_08() override;
+    /* 14 */ virtual void vfunc_14(u32 param1) override;
+    /* 20 */ virtual void vfunc_20(bool param1) override;
+    /* b4 */ virtual unk32 vfunc_b4() = 0;
+    /* b8 */
+
+    bool func_ov014_02135364(unk32 param1);
+    void func_ov014_02135474();
+};
+
+class ActorRefillBombs : public ActorRefill {
+public:
+    /* 000 (base) */
+    /* 168 */
+
+public:
+    ActorRefillBombs();
+    static ActorRefillBombs *Create();
+
+    /* 00 */ virtual ~ActorRefillBombs() override;
+    /* b4 */ virtual unk32 vfunc_b4() override;
+    /* b8 */
+};
+
+class ActorRefillBombchus : public ActorRefill {
+public:
+    /* 000 (base) */
+    /* 168 */
+
+public:
+    ActorRefillBombchus();
+    static ActorRefillBombchus *Create();
+
+    /* 00 */ virtual ~ActorRefillBombchus() override;
+    /* b4 */ virtual unk32 vfunc_b4() override;
+    /* b8 */
+};
+
+class ActorRefillArrows : public ActorRefill {
+public:
+    /* 000 (base) */
+    /* 168 */
+
+public:
+    ActorRefillArrows();
+    static ActorRefillArrows *Create();
+
+    /* 00 */ virtual ~ActorRefillArrows() override;
+    /* b4 */ virtual unk32 vfunc_b4() override;
+    /* b8 */
+};
+
+class ActorRefillTime : public ActorRefill {
+public:
+    /* 000 (base) */
+    /* 168 */
+
+public:
+    ActorRefillTime();
+    static ActorRefillTime *Create();
+
+    /* 00 */ virtual ~ActorRefillTime() override;
+    /* 08 */ virtual bool vfunc_08() override;
+    /* b4 */ virtual unk32 vfunc_b4() override;
+    /* b8 */
+};
+
+class ActorLSTM : public ActorRefill {
+public:
+    /* 000 (base) */
+    /* 168 */
+
+public:
+    ActorLSTM();
+    static ActorLSTM *Create();
+
+    /* 00 */ virtual ~ActorLSTM() override;
+    /* 08 */ virtual bool vfunc_08() override;
+    /* b4 */ virtual unk32 vfunc_b4() override;
+    /* b8 */
+};
---
+++ src/14_Land/Actor/Player/ActorRefill.cpp
@@ -0,0 +1,31 @@
+#include "Actor/Player/ActorRefill.hpp"
+
+ActorRefillBombs *ActorRefillBombs::Create() {}
+ActorRefillBombchus *ActorRefillBombchus::Create() {}
+ActorRefillArrows *ActorRefillArrows::Create() {}
+ActorRefillTime *ActorRefillTime::Create() {}
+ActorLSTM *ActorLSTM::Create() {}
+ActorRefill::ActorRefill() {}
+ActorRefill::~ActorRefill() {}
+bool ActorRefill::vfunc_08() {}
+void ActorRefill::vfunc_14(u32 param1) {}
+bool ActorRefill::func_ov014_02135364(unk32 param1) {}
+void ActorRefill::vfunc_20(bool param1) {}
+void ActorRefill::func_ov014_02135474() {}
+ActorRefillBombs::ActorRefillBombs() {}
+ActorRefillBombs::~ActorRefillBombs() {}
+unk32 ActorRefillBombs::vfunc_b4() {}
+ActorRefillBombchus::ActorRefillBombchus() {}
+ActorRefillBombchus::~ActorRefillBombchus() {}
+unk32 ActorRefillBombchus::vfunc_b4() {}
+ActorRefillArrows::ActorRefillArrows() {}
+ActorRefillArrows::~ActorRefillArrows() {}
+unk32 ActorRefillArrows::vfunc_b4() {}
+ActorRefillTime::ActorRefillTime() {}
+ActorRefillTime::~ActorRefillTime() {}
+bool ActorRefillTime::vfunc_08() {}
+unk32 ActorRefillTime::vfunc_b4() {}
+ActorLSTM::ActorLSTM() {}
+ActorLSTM::~ActorLSTM() {}
+bool ActorLSTM::vfunc_08() {}
+unk32 ActorLSTM::vfunc_b4() {}
